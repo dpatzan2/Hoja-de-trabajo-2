1b26672f6ff38c07e1894e686aa4130626ac2f0e
Diego Patzan <diegopatzan24@gmail.com>
2024-02-01 19:16:17 -0600

    implementing vector model

--- src/interfaces/PostfixEvaluator.java
+++ src/interfaces/PostfixEvaluator.java
@@ -1,4 +1,5 @@
 package interfaces;
 
 public interface PostfixEvaluator {
+    int evaluatePostfix(String expression);
 }
--- src/model/Vector.java
+++ src/model/Vector.java
@@ -1,4 +1,27 @@
 package model;
 
 public class Vector {
+    private int[] array;
+    private int size;
+
+    public Vector(int initialCapacity) {
+        array = new int[initialCapacity];
+        size = 0;
+    }
+
+    public void add(int element) {
+        if (size == array.length) {
+            int[] newArray = new int[size * 2];
+            System.arraycopy(array, 0, newArray, 0, size);
+            array = newArray;
+        }
+        array[size++] = element;
+    }
+
+    public int get(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Index out of bounds");
+        }
+        return array[index];
+    }
 }
